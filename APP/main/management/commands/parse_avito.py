37f101f36fe392afc88109cf8ecef19b016c0f41
Egor <gjyzdby@yandex.ru>
2016-08-02 20:02:31 +0500

    Добавил проверку по линку

--- APP/main/management/commands/parse_avito.py
+++ APP/main/management/commands/parse_avito.py
@@ -22,41 +22,44 @@ class Command(BaseCommand):
         item_list = page.xpath('//*[@class="description"]')
         price_list = page.xpath(
             '//*[@class="popup-prices popup-prices__wrapper clearfix"]/@data-prices')
-        home = []
+        links = [l[0] for l in Apartment.objects.values_list('link')]
         for key, item in enumerate(item_list):
-            title = item.xpath(
-                '//h3[@class="title item-description-title"]/a//text()'
-            )[key]
-
             link = HOST + item.xpath(
                 '//h3[@class="title item-description-title"]/a/@href'
             )[key]
+            if link not in links:
+                title = item.xpath(
+                    '//h3[@class="title item-description-title"]/a//text()'
+                )[key]
+                address = urlopen(link)
+                address = address.read().decode('UTF-8')
+                address = html.fromstring(address)
+                address = address.xpath('//*[@itemprop="streetAddress"]//text()')
+                about = item.xpath(
+                    '//div[@class="about"]'
+                )[key]
+                about = about.text
+                data = item.xpath('//div[@class="data"]')[key]
+                date_t = item.xpath('//div[@class="date c-2"]//text()')[key]
+                date_t = str(date_t).strip()
+                hour_minut = time(int(date_t[-5:-3]), int(date_t[-2:]))
+                date_t = datetime.combine(date.today(), hour_minut).isoformat()
+                price = 0  # Default value
+                if (not about.isspace()) and ((about.strip()[0]).isdigit()):
+                    price = json.loads(price_list.pop(0))
+                    price = price[0]['currencies']['RUB']
 
-            about = item.xpath(
-                '//div[@class="about"]'
-            )[key]
-            about = about.text
-            data = item.xpath('//div[@class="data"]')[key]
-            date_t = item.xpath('//div[@class="date c-2"]//text()')[key]
-            date_t = str(date_t).strip()
-            hour_minut = time(int(date_t[-5:-3]), int(date_t[-2:]))
-            date_t = datetime.combine(date.today(), hour_minut).isoformat()
-            price = 0  # Default value
-            if (not about.isspace()) and ((about.strip()[0]).isdigit()):
-                price = json.loads(price_list.pop(0))
-                price = price[0]['currencies']['RUB']
+                try:
+                    a = Apartment.objects.create(
+                        title=title.strip(),
+                        link=link,
+                        price=price,
+                        date_time=date_t,
+                        city=data[1].text,
+                        agent=str(data[0].text).strip(),
+                        site='Avito',
+                        address=address[0],)
+                    self.stdout.write(self.style.SUCCESS('Successfully'))
+                except Apartment.DoesNotExist:
+                    raise CommandError("Don't create")
 
-            try:
-                a = Apartment.objects.create(
-                    title=title.strip(),
-                    link=link,
-                    price=price,
-                    date_time=date_t,
-                    city=data[1].text,
-                    agent=str(data[0].text).strip(),
-                    street='',
-                    site='HOST')
-            except Apartment.DoesNotExist:
-                raise CommandError("Don't create")
-
-            self.stdout.write(self.style.SUCCESS('Successfully'))
--- APP/main/models.py
+++ APP/main/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 class Apartment(models.Model):
     title = models.CharField(max_length=250)
-    link = models.URLField(max_length=300)
+    link = models.URLField(max_length=300, db_index=True)
     price = models.IntegerField()
     date_time = models.DateTimeField()
     city = models.CharField(max_length=50)
     agent = models.CharField(max_length=300, blank=True)
-    street = models.CharField(max_length=500, blank=True)
     site = models.CharField(max_length=30)
+    address = models.CharField(max_length=50)
 
