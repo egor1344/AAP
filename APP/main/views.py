de20471045cdef3eb3791e709c18ba6afb816e91
Egor <gjyzdby@yandex.ru>
2016-08-05 16:43:25 +0500

    Add simple API

--- APP/main/views.py
+++ APP/main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Apartment
+from .serilializers import ApartmentSerializer
+from rest_framework import viewsets
 
-# Create your views here.
+class ApartmentsViewSet(viewsets.ModelViewSet):
+    """
+    API for apartment
+    """
+    queryset = Apartment.objects.all().order_by('-date_time')
+    serializer_class = ApartmentSerializer
