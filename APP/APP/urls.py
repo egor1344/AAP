aff9f634375aae1571f300636348169408da71bc
Egor <gjyzdby@yandex.ru>
2016-08-07 17:05:46 +0500

    Поменял urls для API

--- APP/APP/urls.py
+++ APP/APP/urls.py
@@ -22,7 +22,7 @@ router = routers.DefaultRouter()
 router.register(r'apartments', ApartmentsViewSet)
 
 urlpatterns = [
-    url(r'^', include(router.urls)),
+    url(r'^api/v1.0/', include(router.urls)),
     url(r'^admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
