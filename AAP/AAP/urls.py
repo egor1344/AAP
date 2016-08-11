21811333eaf1056a59676f616f50f6e52d2b26a4
Egor <gjyzdby@yandex.ru>
2016-08-11 17:54:35 +0500

    Поменял названия папок

--- AAP/AAP/urls.py
+++ AAP/AAP/urls.py
@@ -1,4 +1,4 @@
-"""APP URL Configuration
+"""AAP URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
--- AAP/AAP/wsgi.py
+++ AAP/AAP/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "APP.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "AAP.settings")
 
 application = get_wsgi_application()
