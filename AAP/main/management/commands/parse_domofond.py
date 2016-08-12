40fc393606b34606d3b7a7a2f269aa6330739b70
Egor <gjyzdby@yandex.ru>
2016-08-12 21:46:01 +0500

    Domofond done. Add living space and floor

--- AAP/main/management/commands/parse_domofond.py
+++ AAP/main/management/commands/parse_domofond.py
@@ -22,6 +22,8 @@ class Command(BaseCommand):
         links = [l[0] for l in Apartment.objects.filter(
             site='Domofond').values_list('link')]
         for key, item in enumerate(item_list):
+            self.stdout.write(self.style.SUCCESS('-'*10))
+
             link = HOST + item.xpath('//a[@itemprop="sameAs"]/@href')[key]
             if link not in links:
                 title = item.xpath('//a[@itemprop="sameAs"]/@title')[key]
@@ -35,33 +37,53 @@ class Command(BaseCommand):
                     price_m2 = price_m2.split(' ')
                     price_m2 = price_m2[0].replace('\xa0', '')
                 self.stdout.write(self.style.SUCCESS(price_m2))
-                # agent = ''
-                # try:
-                #     rooms = int(title[0])
-                # except ValueError:
-                #     rooms = 0
-                #     agent = title.split(',')
-                #     agent = agent[-1].strip()
-                # address = item.xpath('//*[@itemprop="address"]//text()')[key]
-                # city = address.split(',')[-2].strip()
-                # price = item.xpath('//*[@itemprop="price"]//text()')[key]
-                # price = price[:-5].replace('\xa0','')
-                # try:
-                #     price = int(price)
-                # except ValueError:
-                #     price = 0
-                # try:
-                #     a = Apartment.objects.create(
-                #             title=title.strip(),
-                #             link=link,
-                #             price=price,
-                #             price_m2=0,
-                #             city=city,
-                #             agent=agent,
-                #             site='Domofond',
-                #             address=address,
-                #             living_space=0,
-                #             rooms=rooms,)
-                #     self.stdout.write(self.style.SUCCESS('Successfully'))
-                # except Apartment.DoesNotExist:
-                #     print('Apartmen dont create ', title)
+                living_space = item.xpath('a/div[1]/div[3]/ul/li[1]/span//text()')
+                self.stdout.write(self.style.SUCCESS(living_space))
+                try:
+                    living_space = living_space.pop(0)
+                except IndexError:
+                    living_space = 0
+                else:
+                    if living_space.find('м²') != -1:
+                        living_space = living_space[:-2]
+                    else:
+                        living_space = 0
+                        floor = living_space 
+                self.stdout.write(self.style.SUCCESS(living_space))
+
+                floor = item.xpath('a/div[1]/div[3]/ul/li[2]/span//text()')
+                try:
+                    floor = floor.pop(0)
+                except IndexError:
+                    floor = 0
+                agent = ''
+                try:
+                    rooms = int(title[0])
+                except ValueError:
+                    rooms = 0
+                    agent = title.split(',')
+                    agent = agent[-1].strip()
+                address = item.xpath('//*[@itemprop="address"]//text()')[key]
+                city = address.split(',')[-2].strip()
+                price = item.xpath('//*[@itemprop="price"]//text()')[key]
+                price = price[:-5].replace('\xa0','')
+                try:
+                    price = int(price)
+                except ValueError:
+                    price = 0
+                try:
+                    a = Apartment.objects.create(
+                            title=title.strip(),
+                            link=link,
+                            price=price,
+                            price_m2=price_m2,
+                            city=city,
+                            agent=agent,
+                            site='Domofond',
+                            address=address,
+                            living_space=living_space,
+                            rooms=rooms,
+                            floor=floor, )
+                    self.stdout.write(self.style.SUCCESS('Successfully'))
+                except Apartment.DoesNotExist:
+                    print('Apartmen dont create ', title)
