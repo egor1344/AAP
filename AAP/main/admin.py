acfc82111bb5d6673f2606d55a8dad3b7d18bb9f
Egor <gjyzdby@yandex.ru>
2016-08-24 17:01:26 +0500

    Update admin list page

--- AAP/main/admin.py
+++ AAP/main/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import Apartment
 
 class ApartmentAdmin(admin.ModelAdmin):
-    list_display = ('title', 'price', 'city', 'date_time')
+    list_display = ('title', 'price', 'city', 'date_time', 'site')
 
 admin.site.register(Apartment, ApartmentAdmin)
